19dcb880ad7f558959463a67ea89981b0757d628
typeJhin <cc.felipemartinez18@gmail.com>
2025-06-02 20:59:59 -0300

    almost finished

--- script/script.js
+++ script/script.js
@@ -65,3 +65,21 @@ window.onscroll = () => {
 
 
 
+const langButtons = document.querySelectorAll("[data-language]")
+const textsToChange = document.querySelectorAll("[data-section]")
+
+langButtons.forEach((button) =>{
+    button.addEventListener("click", () => {
+        fetch(`../languages/${button.dataset.language}.json`)
+            .then (res => res.json())
+            .then (data => {
+                textsToChange.forEach((el) =>{
+                    const section = el.dataset.section;
+                    const value = el.dataset.value;
+
+                    el.innerHTML = data[section][value];
+
+                })
+            })
+    }) 
+})
